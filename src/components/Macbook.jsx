448ad440ad5d16e89380917da8ba49a872f03a65
paras-verma7454 <parasverma7454@gmail.com>
2024-12-01 10:31:46 +0530

    Fixed asset paths and updated Macbook.jsx

--- src/components/Macbook.jsx
+++ src/components/Macbook.jsx
@@ -4,8 +4,8 @@ import React from "react";
 import * as THREE from "three";
 
 export default function Macbook(){
-   let model= useGLTF("./../public/mac.glb");
-   let tex=useTexture("./../public/red.jpg")
+    let model = useGLTF("/mac.glb");
+   let tex=useTexture("/red.jpg")
     let meshes={}
     model.scene.traverse((e)=>{
         meshes[e.name]=e;
